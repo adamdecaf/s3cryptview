2a18eaba098c233c6754f5156b0b23396418508c
Adam Shannon <adam.shannon@banno.com>
2015-05-13 23:25:46 -0500

    build: commenting for future plans

--- bin/build-executables.sh
+++ bin/build-executables.sh
@@ -4,8 +4,4 @@ RELEASE_VERSION=0.1-SNAPSHOT
 sbt assembly
 ln -s target/scala-2.11/s3cryptview-assembly-$RELEASE_VERSION.jar build/s3cryptview-$RELEASE_VERSION.jar
 
-# linux install script
-mkdir -p build/debian
-
-
-mkdir
+# todo: Run tests over built jar?
